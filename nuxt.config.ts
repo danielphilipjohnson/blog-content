4da489488fa32db77287f1ec0a47a364c7714d43
Daniel Philip Johnson <dan@hiyield.co.uk>
2023-03-04 18:19:56 +0000

    Feat: created a basic sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,15 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/prismic", '@funken-studio/sitemap-nuxt-3'],
   // endpoint: 'your_repository_id'
   prismic: { endpoint: "sitemapnuxt3", preview: "/preview" },
+  sitemap: {
+    cacheTime: 1,
+    routes:  () => {
+      return ['contact/', 'blog/'];
+    },
+    defaults: {
+      changefreq: "daily",
+      priority: 1,
+      lastmod: new Date().toISOString()
+    }
+  }
 });
