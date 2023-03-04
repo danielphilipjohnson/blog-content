6febaff3d2669758cc6f7a4fc70ca63925f188e0
Daniel Philip Johnson <dan@hiyield.co.uk>
2023-03-04 17:40:31 +0000

    setup: add module to nuxt config modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
-  modules: ["@nuxtjs/prismic"],
+  modules: ["@nuxtjs/prismic", '@funken-studio/sitemap-nuxt-3'],
   // endpoint: 'your_repository_id'
   prismic: { endpoint: "sitemapnuxt3", preview: "/preview" },
 });
